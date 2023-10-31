baee772db97b83348a2cb17429a77bb546afaa2d
linhapeter <linha.peter.jr@gmail.com>
2023-10-31 22:21:35 +0100

    Routes in separate file and links are generated in loop

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,9 @@
 import React from "react";
-import { BrowserRouter as Router } from "react-router-dom";
 import Header from "./header/Header";
+
 const App = () => (
   <div>
-    <Router>
-      <Header></Header>
-    </Router>
+    <Header></Header>
   </div>
 );
 
--- src/header/Header.jsx
+++ src/header/Header.jsx
@@ -1,57 +1,31 @@
 import React from "react";
-import { Link, Route, Routes } from "react-router-dom";
-import Home from "../pages/home/Home";
-import Examples from "../pages/examples/Examples";
-import Blog from "../pages/blog/Blog";
-import About from "../pages/about/About";
-import Login from "../pages/login/Login";
-import Form from "../pages/examples/form/Form";
-import Chat from "../pages/examples/messenger/Chat";
+import { Link } from "react-router-dom";
 import "../style/header.scss";
+import AppRouting from "../AppRouting";
+
+const routes = [
+  { id: 0, path: "/", label: "Home Page" },
+  { id: 1, path: "/examples", label: "Examples" },
+  { id: 2, path: "/blog", label: "Blog" },
+  { id: 3, path: "/about", label: "About" },
+  { id: 4, path: "/login", label: "Login" },
+];
+
+const renderedLinks = routes.map((route, id) => (
+  <li className="nav-links__li" key={id}>
+    <Link className="nav-links__a" to={route.path}>
+      {route.label}
+    </Link>
+  </li>
+));
 
 const Header = () => {
   return (
     <div>
       <nav className="navbar">
-        <ul className="nav-links">
-          <li className="nav-links__li">
-            <Link className="nav-links__a" to="/">
-              Home Page
-            </Link>
-          </li>
-          <li className="nav-links__li">
-            <Link className="nav-links__a" to="/examples">
-              Examples
-            </Link>
-          </li>
-          <li className="nav-links__li">
-            <Link className="nav-links__a" to="/blog">
-              Blog
-            </Link>
-          </li>
-          <li className="nav-links__li">
-            <Link className="nav-links__a" to="/about">
-              About
-            </Link>
-          </li>
-          <li className="nav-links__li">
-            <Link className="nav-links__a" to="/login">
-              Login
-            </Link>
-          </li>
-        </ul>
+        <ul className="nav-links">{renderedLinks}</ul>
       </nav>
-
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/examples" element={<Examples />}>
-          <Route path="example-1" element={<Form />} />
-          <Route path="example-2" element={<Chat />} />
-        </Route>
-        <Route path="/blog" element={<Blog />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/about" element={<About />} />
-      </Routes>
+      <AppRouting></AppRouting>
     </div>
   );
 };
