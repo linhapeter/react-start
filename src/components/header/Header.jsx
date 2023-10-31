5cd7c78753718097b60b56f65e090c4dfb946925
linhapeter <linha.peter.jr@gmail.com>
2023-10-31 22:27:03 +0100

    File for components created and Header moved there

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import "../style/header.scss";
-import AppRouting from "../AppRouting";
+import "../../style/header.scss";
+import AppRouting from "../../AppRouting";
 
 const routes = [
   { id: 0, path: "/", label: "Home Page" },
